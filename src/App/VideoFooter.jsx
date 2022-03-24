37a8e2e3c1a293c91e348909d0941101c2ccc45a
Xcoderx <xcoder304@gmail.com>
2022-03-24 09:48:11 +0500

    added the footer on the video section

--- src/App/VideoFooter.jsx
+++ src/App/VideoFooter.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import { MdAlbum } from "react-icons/md";
+import { ImMusic } from "react-icons/im";
 const VideoFooter = () => {
-  return <div>VideoFooter</div>;
+  return (
+    <div className="video__footer">
+      <h3 className="video__footer_username">@kaif</h3>
+      <div className="songinfo">
+        <div className="songname">
+          <ImMusic className="musicIcon" />
+          <span>
+            <p>Dua lipa lavtaing-lavtaing-tiktok-remix-2022</p>
+          </span>
+        </div>
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/26/26805.png"
+          className="albumIcon"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default VideoFooter;
--- src/App/Videos.jsx
+++ src/App/Videos.jsx
@@ -1,5 +1,6 @@
 import { useState, useRef } from "react";
 import "./Styles/video.css";
+import VideoFooter from "./VideoFooter";
 
 const Videos = () => {
   const [isVideoPlaying, SetVideoPlaying] = useState(false);
@@ -14,12 +15,13 @@ const Videos = () => {
   return (
     <div className="video">
       <video
-        src="https://v16-webapp.tiktok.com/02570efb826cfbc46a680e4f52eab9b9/623b9536/video/tos/useast2a/tos-useast2a-pve-0037-aiso/0d04584ab33543dda91a67852dede58d/?a=1988&br=1488&bt=744&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-3Aonz7Th9WrMDXq&l=2022032315460701025105822020241FD0&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=MzhlNGU6ZjV1NjMzZjczM0ApaWhlM2g8OTs3NztpZGc5OWdvLjNmcjQwZ2hgLS1kMWNzc2JeMi4yYzAyMi9jXmJfNF46Yw%3D%3D&vl=&vr="
+        src="https://v16-webapp.tiktok.com/34a5c28e5081e638358991a6f7ced3df/623c472b/video/tos/useast2a/tos-useast2a-pve-0037-aiso/0d04584ab33543dda91a67852dede58d/?a=1988&br=1488&bt=744&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-3Aonz7ThbvUMDXq&l=202203240425240102450491931E02AEC3&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=MzhlNGU6ZjV1NjMzZjczM0ApaWhlM2g8OTs3NztpZGc5OWdvLjNmcjQwZ2hgLS1kMWNzc2JeMi4yYzAyMi9jXmJfNF46Yw%3D%3D&vl=&vr="
         onClick={PlayTheVideo}
         ref={VideoPlay}
         loop
         className="video__player"
       />
+      <VideoFooter />
     </div>
   );
 };
